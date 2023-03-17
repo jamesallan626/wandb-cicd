41b16e0f803ef8ffb014306a9b0585092e4c175a
James Allan <jamesallan626@gmail.com>
2023-03-17 15:35:58 -0400

    Update ci.py

--- ci.py
+++ ci.py
@@ -2,7 +2,7 @@ import wandb
 print(f'The version of wandb is: {wandb.__version__}')
 
 # will not pass
-# assert wandb.__version__ == '2.1.01', f'Expected version 2.1.01, but got {wandb.__version__}'
+assert wandb.__version__ == '2.1.01', f'Expected version 2.1.01, but got {wandb.__version__}'
 
 # will pass
-assert wandb.__version__ == '0.14.0', f'Expected version 0.14.0, but got {wandb.__version__}'
+# assert wandb.__version__ == '0.14.0', f'Expected version 0.14.0, but got {wandb.__version__}'
