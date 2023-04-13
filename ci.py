316978e05c88e78d4c1ab80d4a9913d0ad5bdcac
James Allan <jamesallan626@gmail.com>
2023-04-13 11:53:28 -0400

    Update ci.py

--- ci.py
+++ ci.py
@@ -5,4 +5,4 @@ print(f'The version of wandb is: {wandb.__version__}')
 # assert wandb.__version__ == '2.1.01', f'Expected version 2.1.01, but got {wandb.__version__}'
 
 # will pass
-assert wandb.__version__ == '0.14.0', f'Expected version 0.14.0, but got {wandb.__version__}'
+assert wandb.__version__ == '0.14.2', f'Expected version 0.14.2, but got {wandb.__version__}'
